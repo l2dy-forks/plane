d37bb3e114a0d2c9aa241a8c420918d1276e964f
Zero King <l2dy@icloud.com>
2025-05-29 13:12:31 +0000

    chore: restore bulk operation components
    With commands:
    
    git checkout e3ebb9b61cc5ed63919a07f1c389f1e0f2b0efe5^ -- web/core/components/issues/bulk-operations
    rm -r web/ce/components/issues/bulk-operations && git mv web/core/components/issues/bulk-operations web/ce/components/issues/bulk-operations

--- web/ce/components/issues/bulk-operations/upgrade-banner.tsx
+++ web/ce/components/issues/bulk-operations/upgrade-banner.tsx
@@ -1,7 +1,10 @@
 "use client";
 
-import { MARKETING_PLANE_ONE_PAGE_LINK } from "@plane/constants";
+// ui
 import { getButtonStyling } from "@plane/ui";
+// constants
+import { MARKETING_PLANE_ONE_PAGE_LINK } from "@/constants/common";
+// helpers
 import { cn } from "@/helpers/common.helper";
 
 type Props = {
@@ -15,8 +18,7 @@ export const BulkOperationsUpgradeBanner: React.FC<Props> = (props) => {
     <div className={cn("sticky bottom-0 left-0 h-20 z-[2] px-3.5 grid place-items-center", className)}>
       <div className="h-14 w-full bg-custom-primary-100/10 border-[0.5px] border-custom-primary-100/50 py-4 px-3.5 flex items-center justify-between gap-2 rounded-md">
         <p className="text-custom-primary-100 font-medium">
-          Change state, priority, and more for several work items at once. Save three minutes on an average per
-          operation.
+          Change state, priority, and more for several issues at once. Save three minutes on an average per operation.
         </p>
         <a
           href={MARKETING_PLANE_ONE_PAGE_LINK}
